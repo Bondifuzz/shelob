6b4f2ff11ea8bb20d1cf1c69060eb9577c92a7b2
dukebarman <dukebarman@pm.me>
2026-01-03 12:12:09 +0300

    Finish commit

--- response/response.go
+++ response/response.go
@@ -15,25 +15,56 @@ func ParseResponse(ctx context.Context, httpRequests []*http.Request, requestsVa
 	logging.CreateDir(outputDir)
 	log.Debugf("response.go	Total requests to send: %d", len(httpRequests))
 
+	if len(httpRequests) == 0 {
+		log.Debugf("response.go	No requests to send - this might be the issue!")
+		return
+	}
+
 	for idx := range httpRequests {
+		log.Debugf("response.go	About to send request: %s %s", httpRequests[idx].Method, httpRequests[idx].URL.String())
 		func() {
-			log.Debugf("response.go	Sending request: %s %s", httpRequests[idx].Method, httpRequests[idx].URL.String())
 			client := &http.Client{}
-			requestBodyCopy, err := httpRequests[idx].GetBody()
-			if err != nil {
-				log.Warn("response.go	Failed to create copy of body: ", err)
+			// We need to get the body again for logging purposes, but don't consume it unnecessarily
+			// Just get the body for logging if needed
+			var requestBody []byte
+			if httpRequests[idx].GetBody != nil {
+				requestBodyCopy, err := httpRequests[idx].GetBody()
+				if err != nil {
+					log.Warn("response.go	Failed to create copy of body: ", err)
+					// Continue anyway without the body
+					requestBody = []byte{}
+				} else {
+					requestBody, err = io.ReadAll(requestBodyCopy)
+					if err != nil {
+						log.Error("response.go	Failed to read copy of body: ", err)
+						// Continue anyway without the body
+						requestBody = []byte{}
+					}
+				}
+			} else {
+				requestBody = []byte{}
 			}
 
-			requestBody, err := io.ReadAll(requestBodyCopy)
-			if err != nil {
-				log.Error("response.go	Failed to read copy of body: ", err)
-			}
+			log.Debugf("response.go	Sending request: %s %s", httpRequests[idx].Method, httpRequests[idx].URL.String())
+			log.Debugf("response.go	Request URL: %s", httpRequests[idx].URL.String())
+			log.Debugf("response.go	Request method: %s", httpRequests[idx].Method)
+			log.Debugf("response.go	Request headers: %v", httpRequests[idx].Header)
+
+			// Add more debugging to see if the request is actually being sent
+			log.Debugf("response.go	About to make HTTP request to: %s", httpRequests[idx].URL.String())
+			log.Debugf("response.go	Request method: %s", httpRequests[idx].Method)
+			log.Debugf("response.go	Request headers: %v", httpRequests[idx].Header)
+			log.Debugf("response.go	Request body length: %d", len(requestBody))
 
 			httpResponse, err := client.Do(httpRequests[idx])
 			if err != nil {
 				log.Error("response.go	Failed to make http request: ", err)
-				return // Don't fatal on single request failure
+				log.Debugf("response.go	Error details: %v", err)
+				// Instead of returning, continue to next request to allow fuzzing to continue
+				return
 			}
+
+			log.Debugf("response.go	HTTP request sent successfully, got response with status: %d", httpResponse.StatusCode)
 
 			defer httpResponse.Body.Close()
 
@@ -50,9 +81,23 @@ func ParseResponse(ctx context.Context, httpRequests []*http.Request, requestsVa
 			err = ValidateResponse(ctx, requestsValidationInput[idx], responseCode, &responseHeaders)
 
 			if err != nil {
-				logging.WrapCrash(outputDir+"/"+strings.ReplaceAll(httpRequests[idx].URL.RawPath, "/", "_"), httpResponse, *requestsValidationError[idx], requestBody, responseBody, err)
+				log.Debugf("response.go	Validation error for %s %s: %v", httpRequests[idx].Method, httpRequests[idx].URL.String(), err)
+				// Create filename with host:port and path to better identify vulnerable sites
+				hostPort := strings.ReplaceAll(httpRequests[idx].URL.Host, ":", "_") // Replace : with _ to avoid filesystem issues
+				pathPart := strings.ReplaceAll(httpRequests[idx].URL.RawPath, "/", "_")
+				if pathPart == "" || pathPart == "_" {
+					pathPart = "_root" // Use _root for root path to make it more descriptive
+				}
+				logging.WrapCrash(outputDir+"/"+hostPort+pathPart, httpResponse, *requestsValidationError[idx], requestBody, responseBody, err)
 			} else if detailed {
-				logging.WrapTest(outputDir+"/"+strings.ReplaceAll(httpRequests[idx].URL.RawPath, "/", "_"), httpResponse, *requestsValidationError[idx], requestBody, responseBody, err)
+				log.Debugf("response.go	Validation passed for %s %s", httpRequests[idx].Method, httpRequests[idx].URL.String())
+				// Create filename with host:port and path to better identify vulnerable sites
+				hostPort := strings.ReplaceAll(httpRequests[idx].URL.Host, ":", "_") // Replace : with _ to avoid filesystem issues
+				pathPart := strings.ReplaceAll(httpRequests[idx].URL.RawPath, "/", "_")
+				if pathPart == "" || pathPart == "_" {
+					pathPart = "_root" // Use _root for root path to make it more descriptive
+				}
+				logging.WrapTest(outputDir+"/"+hostPort+pathPart, httpResponse, *requestsValidationError[idx], requestBody, responseBody, err)
 			}
 		}()
 	}
